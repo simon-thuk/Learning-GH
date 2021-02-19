f118796cb1577052da4cfb2f10c36dd2752b70f8
Szymon Kocowski <szymon.kocowski@gmail.com>
2021-02-19 21:53:42 +0700

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Szymon Kocowski on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
